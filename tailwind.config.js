909b6d8ece3e6acb0c58bf1cdb7fd3a1acf66e04
taamzid <tamjidalam00@gmail.com>
2024-06-28 21:48:10 +0600

    home-done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      boxShadow: {
+        'custom':
+          "0px 2px 4px 0px rgba(7, 4, 146, 0.10), 0px 24px 60px 0px rgba(6, 47, 125, 0.05), 0px 12px 24px 0px rgba(27, 59, 119, 0.05)",
       },
     },
   },
